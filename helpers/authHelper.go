fb3412633672210038a2959d075795cbe8447c18
PogunGun <pavel.pogulailo@gmail.com>
2023-06-25 12:45:43 +0300

    add auth.helper and token.helper

---
+++ helpers/authHelper.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("Unautorized to access this resource")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userType {
+		err = errors.New("Unautorized to access this resource")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+}
